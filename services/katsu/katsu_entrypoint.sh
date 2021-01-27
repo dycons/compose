906be54afc6602ac977939ac32fecf2c89bd8169
Samarth Agarwal <agarwalsamarth@hotmail.com>
2021-01-27 14:53:51 -0500

    Modified settings.py to enable CORS so that there are no CORS errors when sending requests to the server.

--- services/katsu/katsu_entrypoint.sh
+++ services/katsu/katsu_entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+pip install django-cors-headers
+yes | cp /app/settings.py /app/chord_metadata_service/chord_metadata_service/metadata/settings.py
+
 # This line is necessary in order to give the database that is in another Docker container
 # enough time to finish initializing
 sleep 5
