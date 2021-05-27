a2469dd6b884196d2b3c8dc98de023e333349693
katpavlova <katpavlova.ccm@gmail.com>
2021-05-27 12:10:36 -0400

    Add API key to REMS initialization script

--- services/rems/rems_initialize.sh
+++ services/rems/rems_initialize.sh
@@ -4,3 +4,4 @@ source .env
 docker exec -it rems java -Drems.config=config/config.edn -jar rems.jar api-key add rp
 docker exec -it rems java -Drems.config=config/config.edn -jar rems.jar api-key allow rp get '/api/permissions/.*'
 docker exec -it rems java -Drems.config=config/config.edn -jar rems.jar grant-role owner ${REMS_OWNER_ID}
+docker exec -it rems java -Drems.config=/rems/config/config.edn -jar rems.jar api-key add abc123 Testing
