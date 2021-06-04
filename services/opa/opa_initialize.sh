d0db11c8bfb5c69d5ff8299945918a2f30cce4f0
Samarth Agarwal <agarwalsamarth@hotmail.com>
2021-06-04 17:32:54 -0400

    Moved script to init/ directory.

--- services/opa/opa_initialize.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source .env
-curl -X PUT --data-binary @${OPA_DIR}/data.json localhost:8181/v1/data
-curl -X PUT --data-binary @${OPA_DIR}/passport.rego localhost:8181/v1/policies/ga4ghPassport
